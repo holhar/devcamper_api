cc03fc7cabe37b4f367a45702d4fbb1637433731
Holger Harms <holhar@posteo.de>
2022-01-17 14:10:52 +0100

    Implement ValidationError handling

--- middleware/error.js
+++ middleware/error.js
@@ -11,8 +11,20 @@ const errorHandler = (err, req, res, next) => {
 
     // Mongoose bad ObjectId
     if (err.name === 'CastError') {
-        const message = `Resource not found`;
+        const message = 'Resource not found';
         error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
