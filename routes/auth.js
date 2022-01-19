5e6402a5c40e5074604c2f42559a539eace0e8b2
Holger Harms <holhar@posteo.de>
2022-01-19 17:00:35 +0100

    Implement User model and first auth route

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const { register } = require('../controllers/auth');
+
+const router = express.Router();
+
+router.route('/register').get(register);
+
+module.exports = router;
